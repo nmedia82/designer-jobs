e98ab3bd508e82c06cd5d6bf5cd6ddc4765365f6
CEO-NMEDIA <ceo@najeebmedia.com>
2023-04-16 10:10:33 +0500

    new page assets added

---
+++ src/pages/OurDesigners.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+function OurDesigners() {
+  return (
+    <Container>
+      <h2>Our Designers</h2>
+    </Container>
+  );
+}
+
+export default OurDesigners;
